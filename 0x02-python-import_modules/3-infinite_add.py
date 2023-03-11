701476e6d78ecb138cefd0bb3bafde49b51e11b3
Sophietlc <sophiefanodun@gmail.com>
2023-03-10 16:15:11 -0800

    corrected

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     total = 0
     if (len(sys.argv) > 1):
-        for i in range(1, len(sys.argv)):
-            total += (int(sys.argv[i]))
+        for i, num in enumerate(argv):
+            total += (int(num))
     print("{:d}".format(total))
