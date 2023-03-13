4a7e549e0f7ba08559161bff4dff9b3d3fdbb0de
Sophietlc <sophiefanodun@gmail.com>
2023-03-13 15:59:12 -0700

    first attempt

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list:
+        divisible = []
+        for i in my_list:
+            if i % 2 is 0:
+                divisible.append(True)
+            else:
+                divisible.append(False)
+        return divisible
